4260db28cbf4cf69702bb159f4b193dbe5c224eb
João Victor <joao.victorcoriolano@gmail.com>
2021-11-09 22:25:19 -0300

    Criando classe de dominio Categoria

--- src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.curso.gerenciadorProdutosSpring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.gerenciadorProdutosSpring.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
 	}
 	
 }
