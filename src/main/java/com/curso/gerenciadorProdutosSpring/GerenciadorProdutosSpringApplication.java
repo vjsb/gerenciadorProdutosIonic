e75d8037d0d5d44e8b5b72d9aa9d863b62b5f66b
João Victor <joao.victorcoriolano@gmail.com>
2021-11-09 22:07:01 -0300

    Criação do projeto

---
+++ src/main/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.curso.gerenciadorProdutosSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorProdutosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorProdutosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.gerenciadorProdutosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorProdutosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
