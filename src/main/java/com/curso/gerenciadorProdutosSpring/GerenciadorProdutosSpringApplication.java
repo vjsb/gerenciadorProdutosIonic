84144cf0d51044bdf7c23047a7a06ac628c15794
João Victor <joao.victorcoriolano@gmail.com>
2021-11-11 00:13:47 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplication.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.gerenciadorProdutosSpring.domain.Categoria;
 import com.curso.gerenciadorProdutosSpring.domain.Cidade;
+import com.curso.gerenciadorProdutosSpring.domain.Cliente;
+import com.curso.gerenciadorProdutosSpring.domain.Endereco;
 import com.curso.gerenciadorProdutosSpring.domain.Estado;
 import com.curso.gerenciadorProdutosSpring.domain.Produto;
+import com.curso.gerenciadorProdutosSpring.domain.enums.TipoCliente;
 import com.curso.gerenciadorProdutosSpring.repositories.CategoriaRepository;
 import com.curso.gerenciadorProdutosSpring.repositories.CidadeRepository;
+import com.curso.gerenciadorProdutosSpring.repositories.ClienteRepository;
+import com.curso.gerenciadorProdutosSpring.repositories.EnderecoRepository;
 import com.curso.gerenciadorProdutosSpring.repositories.EstadoRepository;
 import com.curso.gerenciadorProdutosSpring.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class GerenciadorProdutosSpringApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorProdutosSpringApplication.class, args);
@@ -71,6 +82,19 @@ public class GerenciadorProdutosSpringApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 		
 
---
+++ src/main/java/com/curso/gerenciadorProdutosSpring/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.curso.gerenciadorProdutosSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.gerenciadorProdutosSpring.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
