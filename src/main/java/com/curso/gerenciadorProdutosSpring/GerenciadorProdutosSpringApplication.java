c8610e189ca8a143e38997b0f3d7b2e3b6019550
João Victor <joao.victorcoriolano@gmail.com>
2021-11-10 21:34:17 -0300

    Criando operação de instanciação

--- src/main/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplication.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/GerenciadorProdutosSpringApplication.java
@@ -1,13 +1,34 @@
 package com.curso.gerenciadorProdutosSpring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GerenciadorProdutosSpringApplication {
+import com.curso.gerenciadorProdutosSpring.domain.Categoria;
+import com.curso.gerenciadorProdutosSpring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class GerenciadorProdutosSpringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorProdutosSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Escritório");
+		Categoria cat2 = new Categoria(null, "Informática");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
