b8fc567738f373276910c1cb13a5ba1d0d3c2843
João Victor <joao.victorcoriolano@gmail.com>
2021-11-18 17:57:31 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.curso.gerenciadorProdutosSpring.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.gerenciadorProdutosSpring.domain.Categoria;
 import com.curso.gerenciadorProdutosSpring.services.CategoriaService;
@@ -23,15 +24,16 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) throws Exception {
 		Categoria obj = service.find(id);
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> listaCategorias = new ArrayList<>();
-		listaCategorias.add(cat1);
-		listaCategorias.add(cat2);
-		
 		return ResponseEntity.ok(obj);
+	}
+	
+	//requestBody faz o JSON ser convertido para objeto Java automaticamente
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
