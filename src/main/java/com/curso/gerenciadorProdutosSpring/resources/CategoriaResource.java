8a56547bb115a58b8ddcdd3492a55e13c4891196
João Victor <joao.victorcoriolano@gmail.com>
2021-11-23 14:12:26 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,48 +25,57 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) throws Exception {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
-	
-	
-	//requestBody faz o JSON ser convertido para objeto Java automaticamente
+
+	// requestBody faz o JSON ser convertido para objeto Java automaticamente
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	
-	//recebe o PathVariable e o RequestBody por ter que pegar um objeto e atualiza-lo
+
+	// recebe o PathVariable e o RequestBody por ter que pegar um objeto e
+	// atualiza-lo
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) throws Exception{
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) throws Exception {
 		obj.setId(id);
 		obj = service.update(obj);
-		//quando se atualiza com sucesso retorna um 204(NoContent)
+		// quando se atualiza com sucesso retorna um 204(NoContent)
 		return ResponseEntity.noContent().build();
 	}
-	
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete (@PathVariable Integer id) throws Exception {
+	public ResponseEntity<Void> delete(@PathVariable Integer id) throws Exception {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	//retorna todas as categorias, no caso DTO porque só queremos o id e o nome dels e não seus produtos
+
+	// retorna todas as categorias, no caso DTO porque só queremos o id e o nome
+	// dela e não seus produtos
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() throws Exception {
 		List<Categoria> list = service.findAll();
-		//com essa lista convertemos uma lista de Categoria para CategoriaDTO
+		// com essa lista convertemos uma lista de Categoria para CategoriaDTO
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok(listDto);
 	}
-	
-	
+
+	// retorna uma pagina contendo os dados
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) throws Exception {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok(listDto);
+	}
+
 }
--- src/main/java/com/curso/gerenciadorProdutosSpring/services/CategoriaService.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.curso.gerenciadorProdutosSpring.domain.Categoria;
@@ -51,6 +54,14 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}
+	
+	//esse método retorna uma pagina com uma contagem de dados
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return categoriaRepository.findAll(pageRequest);
+
 	}
 	
 }
