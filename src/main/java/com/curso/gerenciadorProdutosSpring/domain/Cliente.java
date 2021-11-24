4deb84b550b10c2af86cac79969f29cbfed7fbf8
João Victor <joao.victorcoriolano@gmail.com>
2021-11-23 23:30:27 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/curso/gerenciadorProdutosSpring/domain/Cliente.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -33,7 +34,8 @@ public class Cliente implements Serializable {
 	
 	private Integer tipoCliente;
 	
-	@OneToMany(mappedBy = "cliente")
+	// cascade para caso apague o cliente apague os endereços junto
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	/*
