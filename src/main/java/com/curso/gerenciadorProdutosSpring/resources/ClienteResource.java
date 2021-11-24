c4de6d8172d8d96f4ac7afbfb26060a36d78242d
João Victor <joao.victorcoriolano@gmail.com>
2021-11-23 23:25:16 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/curso/gerenciadorProdutosSpring/resources/ClienteResource.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.curso.gerenciadorProdutosSpring.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.gerenciadorProdutosSpring.domain.Cliente;
 import com.curso.gerenciadorProdutosSpring.dto.ClienteDTO;
+import com.curso.gerenciadorProdutosSpring.dto.ClienteNewDTO;
 import com.curso.gerenciadorProdutosSpring.services.ClienteService;
 
 @RestController
@@ -30,6 +33,14 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) throws Exception {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/curso/gerenciadorProdutosSpring/services/ClienteService.java
+++ src/main/java/com/curso/gerenciadorProdutosSpring/services/ClienteService.java
@@ -3,16 +3,23 @@ package com.curso.gerenciadorProdutosSpring.services;
 import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.curso.gerenciadorProdutosSpring.domain.Cidade;
 import com.curso.gerenciadorProdutosSpring.domain.Cliente;
+import com.curso.gerenciadorProdutosSpring.domain.Endereco;
+import com.curso.gerenciadorProdutosSpring.domain.enums.TipoCliente;
 import com.curso.gerenciadorProdutosSpring.dto.ClienteDTO;
+import com.curso.gerenciadorProdutosSpring.dto.ClienteNewDTO;
 import com.curso.gerenciadorProdutosSpring.repositories.ClienteRepository;
+import com.curso.gerenciadorProdutosSpring.repositories.EnderecoRepository;
 import com.curso.gerenciadorProdutosSpring.resources.exception.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -22,11 +29,22 @@ public class ClienteService {
 
 	@Autowired
 	ClienteRepository clienteRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) throws Exception {
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		clienteRepository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) throws Exception {
@@ -59,6 +77,24 @@ public class ClienteService {
 
 	public Cliente fromDto(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	//sobrecarga do método fromDto para criação de um novo Cliente
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+		
+		
 	}
 	
 	//atualizar o objeto newObj com os dados que vieram de obj
